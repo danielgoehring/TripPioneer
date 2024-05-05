3acb2674fa46455913a79ba87073737d5e98874a
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-05-05 15:16:17 -0400

    added more styles to the search bar

--- frontend/src/features/SearchBar.jsx
+++ frontend/src/features/SearchBar.jsx
@@ -5,26 +5,44 @@ function SearchBar() {
 
   return (
     <>
-      <div className="px-80">
-        <div className="border shadow-lg py-2 px-8 rounded-full mt-10">
+      <div className="px-96">
+        <div className="border shadow-lg py-2 pl-8 pr-2 rounded-full mt-10">
           <div className="flex">
             <div>
               <label>Destination</label>
-              <input />
+              <input placeholder="Find destinations" />
             </div>
-            <div>
-              <label>Check in time</label>
-              <input />
+            <div className="border-l border-l-2 border-l-gray-100 ">
+              <div className="ml-4">
+                <label>Check in time</label>
+                <input placeholder="Add check in dates" />
+              </div>
             </div>
-            <div>
-              <label>Check out time</label>
-              <input />
+            <div className="border-l border-l-2 border-l-gray-100">
+              <div className="ml-4">
+                <label>Check out time</label>
+                <input placeholder="Add check out dates" />
+              </div>
             </div>
-            <div>
-              <label>Guests</label>
-              <input />
+            <div className="border-l border-l-2 border-l-gray-100">
+              <div className="ml-4">
+                <label>Guests</label>
+                <input placeholder="Add guests" />
+              </div>
             </div>
-            <button>search</button>
+
+            <button className="border rounded-full bg-emerald-500 p-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-search text-white"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
