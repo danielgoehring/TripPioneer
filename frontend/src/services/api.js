ddfb7702773d5d7f82a1f812a7355dc6b97b58bb
danielgoehring <69319848+danielgoehring@users.noreply.github.com>
2024-08-27 14:12:17 -0400

    updated frontend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchImages = async () => {
   try {
-    const response = await axios.get("http://localhost:5002/images");
+    const response = await axios.get("https://trippioneer.onrender.com");
     return response.data;
   } catch (error) {
     console.error("Error fetching images:", error);
